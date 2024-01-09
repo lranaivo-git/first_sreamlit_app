fffab116180146c9815c856ff06e05b2e8b87d5e
lranaivo-git <156125338+lranaivo-git@users.noreply.github.com>
2024-01-09 23:37:57 +0100

    Update streamlit_app.py
    Add emojiis in menu itzm

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled Free Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard boiled Free Range Egg')
 
