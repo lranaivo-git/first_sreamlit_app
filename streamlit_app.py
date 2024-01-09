5d0ad51a557e889e670cd7c3e4d14be3082313ac
lranaivo-git <156125338+lranaivo-git@users.noreply.github.com>
2024-01-09 23:45:41 +0100

    Update streamlit_app.py
    ADd a pick list to select the fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,11 @@ streamlit.text('🍞 Hard boiled Free Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#load data from s3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display table
 streamlit.dataframe(my_fruit_list)
