1a01904351bfbc5ce3ec1bacac42fcfd5c2af738
lranaivo-git <156125338+lranaivo-git@users.noreply.github.com>
2024-01-10 15:15:34 +0100

    Update streamlit_app.py
    insert selected valules into SF db

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def get_fruit_load_list():
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from Streamlit')")
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('" + new_fruit +"')")
     return "Thanks for adding " + new_fruit
 
 streamlit.title('My parents new healthy Diner')
