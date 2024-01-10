6cc498efb0cf483571a1e364f4e3ac6722ed4ca2
lranaivo-git <156125338+lranaivo-git@users.noreply.github.com>
2024-01-10 14:15:00 +0100

    Update streamlit_app.py
    use dataframe to show snowflake loaded data

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen
 streamlit.dataframe(fruityvice_normalized)
 
+#snowflake content
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fuit load list conatains")
+
+streamlit.dataframe(my_data_row)
