93f497f2a21c127b6060800f5422d8a3a7ee48b3
lranaivo-git <156125338+lranaivo-git@users.noreply.github.com>
2024-01-10 03:35:26 +0100

    Update streamlit_app.py
    Add text box and add variablefor fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,12 @@ streamlit.dataframe(fruits_to_show)
 
 #display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# text box to enter fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # Take the json response and normalize it
